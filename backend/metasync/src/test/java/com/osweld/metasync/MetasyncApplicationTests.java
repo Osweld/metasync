9ef42b1ab74a0f3fc334fe732215cf9ac75933bd
German Reyes <oswaldo101.gr@gmail.com>
2026-01-08 21:47:20 -0600

    feat: initialize Spring Boot application with Maven wrapper and configuration files

---
+++ backend/metasync/src/test/java/com/osweld/metasync/MetasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osweld.metasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
