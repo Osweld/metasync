a805eb469a8b9c1420e87ac5021bb9fc98d05ede
German Reyes <oswaldo101.gr@gmail.com>
2026-01-23 19:48:33 -0600

    feat: remove tenant-related model classes for refactoring

--- backend/metasync/src/main/java/com/osweld/metasync/tenant/internal/domain/model/tenant/PlanType.java
+++
@@ -1,7 +0,0 @@
-package com.osweld.metasync.tenant.internal.domain.model.tenant;
-
-public enum PlanType {
-    FREE,
-    PLUS,
-    PREMIUM
-}
--- backend/metasync/src/main/java/com/osweld/metasync/tenant/internal/domain/model/tenant/StatusType.java
+++
@@ -1,9 +0,0 @@
-package com.osweld.metasync.tenant.internal.domain.model.tenant;
-
-public enum StatusType {
-    ACTIVE,
-    INACTIVE,
-    SUSPENDED,
-    CANCELLED,
-    PENDING
-}
