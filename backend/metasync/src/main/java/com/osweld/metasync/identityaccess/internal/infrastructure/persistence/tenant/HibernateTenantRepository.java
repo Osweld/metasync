858abd1ee81724774fc58d1c51e67ed6b748bee7
German Reyes <oswaldo101.gr@gmail.com>
2026-02-06 22:11:01 -0600

    feat: implement HibernateTenantRepository for tenant management

---
+++ backend/metasync/src/main/java/com/osweld/metasync/identityaccess/internal/infrastructure/persistence/tenant/HibernateTenantRepository.java
@@ -0,0 +1,45 @@
+package com.osweld.metasync.identityaccess.internal.infrastructure.persistence.tenant;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.osweld.metasync.identityaccess.internal.domain.model.tenant.Tenant;
+import com.osweld.metasync.identityaccess.internal.domain.model.tenant.TenantAlias;
+import com.osweld.metasync.identityaccess.internal.domain.model.tenant.TenantId;
+import com.osweld.metasync.identityaccess.internal.domain.model.tenant.TenantRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class HibernateTenantRepository implements TenantRepository {
+
+    private final TenantJpaRepository tenantJpaRepository;
+    private final TenantMapper tenantMapper;
+
+    @Override
+    public TenantId nextIdentity() {
+       return TenantId.generate();
+    }
+
+    @Override
+    public void save(Tenant tenant) {
+        TenantJpaEntity entity = tenantMapper.toJpaEntity(tenant);
+        tenantJpaRepository.save(entity);
+    }
+
+    @Override
+    public Optional<Tenant> findById(TenantId tenantId) {
+        return tenantJpaRepository.findById(tenantId.value())
+                .map(tenantMapper::toDomainModel);
+    }
+
+    @Override
+    public boolean existsByTenantAlias(TenantAlias tenantAlias) {
+        return tenantJpaRepository.existsByTenantAlias(tenantAlias.value());
+    }
+
+    
+
+}
