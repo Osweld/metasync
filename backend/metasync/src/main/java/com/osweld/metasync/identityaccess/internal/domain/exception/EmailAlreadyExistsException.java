c68d4a201ba1f918b4c9e4ade79bd22f24436a77
German Reyes <oswaldo101.gr@gmail.com>
2026-02-06 22:10:10 -0600

    feat: extend EmailAlreadyExistsException to inherit from IdentityAccessDomainException

--- backend/metasync/src/main/java/com/osweld/metasync/identityaccess/internal/domain/exception/EmailAlreadyExistsException.java
+++ backend/metasync/src/main/java/com/osweld/metasync/identityaccess/internal/domain/exception/EmailAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.osweld.metasync.identityaccess.internal.domain.exception;
 
-public class EmailAlreadyExistsException {
+public class EmailAlreadyExistsException extends IdentityAccessDomainException {
+
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
 
 }
