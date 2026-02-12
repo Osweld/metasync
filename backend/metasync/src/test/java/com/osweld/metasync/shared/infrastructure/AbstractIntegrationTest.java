db099f2de3933ffec5ebd79055ac5cefd6669e65
German Reyes <oswaldo101.gr@gmail.com>
2026-02-11 22:09:45 -0600

    feat: add AbstractIntegrationTest class for PostgreSQL integration testing setup

---
+++ backend/metasync/src/test/java/com/osweld/metasync/shared/infrastructure/AbstractIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.osweld.metasync.shared.infrastructure;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class AbstractIntegrationTest {
+
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.liquibase.parameters.POSTGRES_USER", postgres::getUsername);
+    }
+
+}
