4f446e6478637d70577e5de52d1b5f663988dee8
German Reyes <oswaldo101.gr@gmail.com>
2026-02-13 20:30:16 -0600

    fix: remove redundant Liquibase user parameter from integration test properties

--- backend/metasync/src/test/java/com/osweld/metasync/shared/infrastructure/AbstractIntegrationTest.java
+++ backend/metasync/src/test/java/com/osweld/metasync/shared/infrastructure/AbstractIntegrationTest.java
@@ -17,7 +17,6 @@ public class AbstractIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.liquibase.parameters.POSTGRES_USER", postgres::getUsername);
     }
 
 }
