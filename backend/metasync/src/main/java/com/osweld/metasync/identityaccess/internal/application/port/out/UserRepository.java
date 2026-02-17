65fc5995e96d32414b90fce6bbc7c71898d10e09
German Reyes <oswaldo101.gr@gmail.com>
2026-02-17 17:45:38 -0600

    feat: move UserRepository interface to a new package for better organization

---
+++ backend/metasync/src/main/java/com/osweld/metasync/identityaccess/internal/application/port/out/UserRepository.java
@@ -0,0 +1,16 @@
+package com.osweld.metasync.identityaccess.internal.application.port.out;
+
+import java.util.Optional;
+
+import com.osweld.metasync.identityaccess.internal.domain.model.user.EmailAddress;
+import com.osweld.metasync.identityaccess.internal.domain.model.user.User;
+import com.osweld.metasync.identityaccess.internal.domain.model.user.UserId;
+
+public interface UserRepository {
+
+    UserId nextIdentity();
+    void save(User user);
+    Optional<User> findById(UserId userId);
+    boolean existsByEmail(EmailAddress emailAddress);
+
+}
