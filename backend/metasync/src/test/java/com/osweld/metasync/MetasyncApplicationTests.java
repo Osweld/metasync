1aaa5aa16f9749601a76733e82750fded31a9681
German Reyes <oswaldo101.gr@gmail.com>
2026-02-19 17:30:35 -0600

    feat: extend MetasyncApplicationTests to inherit from AbstractIntegrationTest

--- backend/metasync/src/test/java/com/osweld/metasync/MetasyncApplicationTests.java
+++ backend/metasync/src/test/java/com/osweld/metasync/MetasyncApplicationTests.java
@@ -1,10 +1,11 @@
 package com.osweld.metasync;
 
+import com.osweld.metasync.shared.infrastructure.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MetasyncApplicationTests {
+class MetasyncApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
