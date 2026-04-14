2e576fbaa3d1ed0bf8f5e36b69f81d6bd41fa669
German Reyes <oswaldo101.gr@gmail.com>
2026-04-14 08:32:05 -0600

    feat: add expiration time to TokenType enum for token management

--- backend/metasync/src/main/java/com/osweld/metasync/identityaccess/internal/domain/model/token/account/TokenType.java
+++ backend/metasync/src/main/java/com/osweld/metasync/identityaccess/internal/domain/model/token/account/TokenType.java
@@ -1,7 +1,18 @@
 package com.osweld.metasync.identityaccess.internal.domain.model.token.account;
 
+import lombok.Getter;
+
+@Getter
 public enum TokenType {
-    PASSWORD_RESET,
-    EMAIL_VERIFICATION,
-    TENANT_ACTIVATION
+    PASSWORD_RESET(15L),
+    EMAIL_VERIFICATION(60L),
+    TENANT_ACTIVATION(60L);
+
+    private final Long expirationMinutes;
+
+    TokenType(Long expirationMinutes) {
+        this.expirationMinutes = expirationMinutes;
+    }
+
+
 }
